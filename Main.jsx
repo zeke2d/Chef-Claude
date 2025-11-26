a0a6b1143fef3266adbffbc573991290e0bd77f9
zeke2d <129525308+zeke2d@users.noreply.github.com>
2025-11-26 13:22:36 +0800

    conditional rendering get-recipe-container
    +  Only displays the div get-recipe-container if the ingredients list has more than 3 ingredients in it.
    * doing this so there's more ingredients for ze "chef" to work with

--- Main.jsx
+++ Main.jsx
@@ -24,17 +24,16 @@ export default function Main() {
                 />
                 <button>Add ingredient</button>
             </form>
-            {/* conditional rendering, only render the new <section> IF there are ingredients added to the list of ingredients.*/}
             {ingredients.length > 0 && <section>
                 <h2>Ingredients on hand:</h2>
                 <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
-                <div className="get-recipe-container">
+                {ingredients.length > 3 && <div className="get-recipe-container">
                     <div>
                         <h3>Ready for a recipe?</h3>
                         <p>Generate a recipe from your list of ingredients.</p>
                     </div>
                     <button>Get a recipe</button>
-                </div>
+                </div>}
             </section>}
         </main>
     )
