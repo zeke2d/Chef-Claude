e0b533f776a489bd1517d80842a2dc4235a4f665
zeke2d <129525308+zeke2d@users.noreply.github.com>
2025-11-26 12:55:43 +0800

    conditional rendering new section
    + new section
    + conditionally render section
    + new styles for section

--- Main.jsx
+++ Main.jsx
@@ -24,9 +24,18 @@ export default function Main() {
                 />
                 <button>Add ingredient</button>
             </form>
-            <ul>
-                {ingredientsListItems}
-            </ul>
+            {/* conditional rendering, only render the new <section> IF there are ingredients added to the list of ingredients.*/}
+            {ingredients.length > 0 && <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            </section>}
         </main>
     )
 }
