0201afe2e99dd7dab418c8f1ee0238676d507eb5
zeke2d <129525308+zeke2d@users.noreply.github.com>
2025-12-11 01:15:51 +0800

    add gif and quote below recipe markdown
    + gif and quote below markdown
    + repeating gradient styling for quote
    + faded edges styling for gif to give dreamlike quality

--- components/ClaudeRecipe.jsx
+++ components/ClaudeRecipe.jsx
@@ -1,10 +1,15 @@
 import Markdown from 'marked-react';
+import ChefSanjiCooking from '../images/chef-sanji-cooking.gif';
 
 export default function ClaudeRecipe(props) {
     return (
         <section className="suggested-recipe-container" aria-live="polite">
             <h2>Zeee Chef Recommends:</h2>
             <Markdown>{props.recipe}</Markdown>
+            <div className="gif-quote-container">
+                <img src={ChefSanjiCooking} id="chef-sanji-cooking" alt="Chef Sanji cooking GIF." />
+                <q id="chef-sanji-quote">Every cook has a duty to make sure each little ingredient is used with love</q>
+            </div>
         </section>
     )
 }
