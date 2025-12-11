1f14019d029a8ef2b4f64a94b6d4e71bfcc03d93
zeke2d <129525308+zeke2d@users.noreply.github.com>
2025-12-11 23:30:07 +0800

    replace marked-react with marked + DOMPurify
    **Dependency conflict**
    
    * npm uninstall marked-react
    + npm install marked dompuriy

--- components/AIRecipe.jsx
+++ components/AIRecipe.jsx
@@ -1,13 +1,16 @@
-import Markdown from 'marked-react';
+import { marked } from "marked";
+import DOMPurify from "dompurify";
 import ChefSanjiCooking from '../assets/chef-sanji-cooking.gif';
 
 export default function AIRecipe(props) {
+    const html = DOMPurify.sanitize(marked.parse(props.recipe || ""));
+
     return (
         <section className="suggested-recipe-container" aria-live="polite">
             <h2>
                 <span className="gradient-text">Chef Sunny</span> Recommends:
             </h2>
-            <Markdown>{props.recipe}</Markdown>
+            <div dangerouslySetInnerHTML={{ __html: html }} />
             <div className="gif-quote-container">
                 <img src={ChefSanjiCooking} id="chef-sanji-cooking" alt="Chef Sanji cooking GIF." />
                 <q id="chef-sanji-quote">Every cook has a duty to make sure each little ingredient is used with love</q>
